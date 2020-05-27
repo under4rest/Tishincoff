a1ffc92964dd7a12efc5a7299e466c895a31c8f5
mikepodlesny <mikepodlesny@mail.ru>
2020-05-27 18:25:13 +0300

    half of react-redux

--- src/Screens/UnderConstruct/UnderConstruct.js
+++ src/Screens/UnderConstruct/UnderConstruct.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './master.css'
 import Cup from './img/black_pink.png';
+import { connect, bindActionCreators } from 'react-redux'
 
 class UnderConstruct extends React.Component {
   render() {
-    console.warn(this.props);
-    
+    const { coffee, addToCart } = this.props
     return (
       <>
         <div className="holder">
           <h6 className="under_construction_h6">Угадай новый рецепт</h6>
           <h1 className="under_construction">Готовим блокбастер</h1>
           <img src={Cup} alt='website logo' className="cup" />
-          <div className="main-button" onClick={() => this.props.data.functions.addToCart(this.props.data.coffee[0])}>хочу добавить первый товар</div>
+          <div className="main-b÷utton" onClick={() => addToCart(coffee[0])}>хочу добавить первый товар</div>
           <div className="main-button" onClick={() => this.props.data.functions.addToCart(this.props.data.coffee[1])}>хочу добавить второй товар</div>
           <div className="main-button" onClick={() => this.props.data.functions.delToCart(this.props.data.coffee[0])}>хочу удалить первый товар</div>
           <div className="main-button" onClick={() => this.props.data.functions.delToCart(this.props.data.coffee[1])}>хочу удалить второй товар</div>
@@ -21,5 +21,18 @@ class UnderConstruct extends React.Component {
     )
   };
 }
+const mapStateToPorps = (state) => {
+  console.warn(state)
+  return {
+    coffee: state.coffee,
+  }
+};
+const increment = () => ({ type: 'ACTION_ADD_TO_CART'})
 
-export default UnderConstruct
+const mapDispatchToProps = (dispatch) => {
+  console.warn(dispatch);
+  return {
+    addToCart: (e) => dispatch(increment(e))
+  }
+}
+export default connect(mapStateToPorps, mapDispatchToProps)(UnderConstruct)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,89 @@
+/* eslint-disable default-case */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import state from './Redux/state';
+import { Provider } from 'react-redux'
+import { createStore, bindActionCreators } from 'redux';
+
+const initState = {
+  coffee: [
+    {
+      good_id: 0,
+      good_name: 'Тестовый кофе',
+      good_description: 'Тестовое описание кофе'
+    },
+    {
+      good_id: 1,
+      good_name: 'Тестовый кофе',
+      good_description: 'Тестовое описание кофе'
+    },
+    {
+      good_id: 2,
+      good_name: 'Тестовый кофе',
+      good_description: 'Тестовое описание кофе'
+    },
+  ],
+  cart: [],
+  functions: {
+    delToCart: (e) => {
+      let exist = null
+      state.cart.forEach((item, index) => {
+        if (item.good_id === e.good_id) {
+          exist = index
+          console.warn(exist, index);
+        }
+      })
+      if (exist != null) { state.cart.splice(exist, 1) }
+      console.warn('нажал на кнопку удаления из корзины', state.cart);
+    },
+    addToCart: (e) => {
+      let exist = null
+      let f = {
+        good_id: e.good_id,
+        good_name: e.good_name,
+        good_count: 1,
+      }
+      state.cart.forEach((item, index) => {
+        if (item.good_id === e.good_id) {
+          exist = index
+          console.warn(exist, index);
+        }
+      })
+      if (exist != null) { state.cart[exist].good_count++ } else { state.cart.push(f) }
+      console.warn('нажал на кнопку довления в корзину', state.cart);
+    }
+  }
+}
+
+
+const ACTION_ADD_TO_CART = 'ACTION_ADD_TO_CART'
+
+const actionAddToCart = (e) => {
+  return {
+    type: ACTION_ADD_TO_CART,
+    payload: e,
+  }
+}
+const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case ACTION_ADD_TO_CART:
+      return { ...state, cart: action.payload }
+  }
+  return state
+}
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const mapDispatchToProps = {
+  actionAddToCart
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App state={state}/>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App state={state} />
+  </Provider>,
   document.getElementById('root')
 );
 
