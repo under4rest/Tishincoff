dc0115ede20b66b1cf0755dcc558b9753f3b9e58
mikepodlesny <mikepodlesny@mail.ru>
2020-05-28 19:04:32 +0300

    container fix

--- src/Screens/ProductListScreen/ProductList.js
+++ src/Screens/ProductListScreen/ProductList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import { Container } from 'react-bootstrap'
 import ProductCard from './../../Components/ProductCard/ProductCard'
 import actionAddToCart, { actionDelFromCart } from './../../Redux/actions/addToCart'
 
@@ -10,19 +10,22 @@ class ProductList extends React.Component {
     const { coffee, cart, addToCart, delFromCart } = this.props
     return (
       <div className="holder">
-        <h1 className="under_construction">Товары</h1>
-        <h6 className="under_construction_h6">Весь список товаров хранится на сервере и генерируется автоматически</h6>
-        {coffee.map((element, key) => {
-          return (
-            <ProductCard cart={cart} element={element} addToCart={addToCart} delFromCart={delFromCart} />
-          )
-        })
-        }
-        <h1 className="under_construction">Корзина</h1>
-        <h6 className="under_construction_h6">Это не просто переключатель кнопок, а хранилище состояние корзины которе можно достать из любого места</h6>
-        {cart.map(item => {
-          return <h3>{item.good_name} c id {item.good_id} в корзине</h3>
-        })}
+        <Container>
+
+          <h1 className="under_construction">Товары</h1>
+          <h6 className="under_construction_h6">Весь список товаров хранится на сервере и генерируется автоматически</h6>
+          {coffee.map((element, key) => {
+            return (
+              <ProductCard cart={cart} element={element} addToCart={addToCart} delFromCart={delFromCart} />
+            )
+          })
+          }
+          <h1 className="under_construction">Корзина</h1>
+          <h6 className="under_construction_h6">Это не просто переключатель кнопок, а хранилище состояние корзины которе можно достать из любого места</h6>
+          {cart.map(item => {
+            return <h3>{item.good_name} c id {item.good_id} в корзине</h3>
+          })}
+        </Container>
       </div>
     )
   }
