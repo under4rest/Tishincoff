1fc340395ce5986bb291f577eaac6edaa51ace93
mikepodlesny <mikepodlesny@mail.ru>
2020-06-22 17:57:04 +0300

    logger

--- server/index.js
+++ server/index.js
@@ -3,14 +3,27 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 
+
 const port = process.env.PORT || 80;
 const app = express();
 
 const root = '../build/';
 app.use(express.static(root));
 app.use((req, res) => {
+  var ip;
+  if (req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(",")[0];
+  } else if (req.connection && req.connection.remoteAddress) {
+    ip = req.connection.remoteAddress;
+  } else {
+    ip = req.ip;
+  } console.log("|" + ip + "|");
   fs.createReadStream(path.join(root, 'index.html')).pipe(res);
 })
 
+
 const server = http.createServer(app);
-server.listen(port, () => console.log('server run at port', port));
+server.listen(port, () => {
+  console.log('server run at port', port)
+}
+)
